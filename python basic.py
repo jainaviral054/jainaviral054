ec0c6635d675f997d11633a5065b611ee1b07b0e
Aviral Jain <99413870+jainaviral054@users.noreply.github.com>
2022-12-30 00:33:46 +0530

    Add files via upload

---
+++ python basic.py
@@ -0,0 +1,21 @@
+a=3   # value 1
+b=5   #value 2
+print(a+b)   # Addition
+print(a-b)   # Substraction
+print(a/b)   # Division
+print(a*b)   # Multiply
+print(a%b)   # Reminder
+print(a**b)
+
+print ("hello world")  #first code in python
+a=1   
+b=2
+c=3
+print ((a+b)/c)
+
+
+
+
+
+
+
